5e3fe5affb6064993a6388189cf132a375771ff9
epireve <i@firdaus.my>
2025-04-19 18:23:31 +0800

    Fix styling issues and add scripts to run both frontend and backend

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "./styles.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { QueryProvider } from "@/components/providers/query-provider";
 import { Navbar } from "@/components/custom/navbar";
